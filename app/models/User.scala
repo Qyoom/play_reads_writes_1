62a716ff9948a9147f188ac028686d6da4114d88
GitOutATown <richard@aqume.com>
2014-09-27 19:11:28 -0700

    Removed duplicate internal implicit Writes from User model

--- app/models/User.scala
+++ app/models/User.scala
@@ -2,16 +2,4 @@ package models
 import play.api.libs.json.Writes
 import play.api.libs.json.Json
 
-case class User(id: Long, name: String, friends: Seq[User]) {
-
-	implicit val userWrites = new Writes[User] {    
-	    def writes(u: User) = Json.obj(
-	    		"id" -> u.id,
-	    		"name" -> u.name,
-	    		"friends" -> u.friends.map(fr => Json.obj(
-		        "id" -> fr.id,
-		        "name" -> fr.name
-	    		))
-    		)
-	}
-}
+case class User(id: Long, name: String, friends: Seq[User])
